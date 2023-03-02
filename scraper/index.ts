8bc4fc11d0d4282614b8a2a41af9819d8ac93e20
hdntc <realcirno@protonmail.com>
2023-03-02 14:20:38 +0000

    larger screenshot

--- scraper/index.ts
+++ scraper/index.ts
@@ -5,6 +5,7 @@ import * as puppeteer from "puppeteer";
     const newPage = await browser.newPage();
 
     await newPage.goto("https://cityworks.jp");
+    await newPage.setViewport({ width: 1920, height: 1080 });
     await newPage.screenshot({ path: "./screenshots/homepage.png" });
 
     await browser.close();
