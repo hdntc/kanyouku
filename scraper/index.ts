17b487243f2757189d6006308f061b3d1f92b676
hdntc <realcirno@protonmail.com>
2023-03-03 18:36:49 +0000

    write comments

--- scraper/index.ts
+++ scraper/index.ts
@@ -44,17 +44,19 @@ const normalizeDakuten = (unnorm: string) => {
     await newPage.goto("https://cityworks.jp");
     const entries = (await newPage.waitForSelector("#list")) as unknown as HTMLDivElement;
 
-    const rawPhrases: string[] = await newPage.evaluate(val => {
-        const matches = val.textContent?.matchAll(/「[^「」]*」/g);
+    const rawPhrases: string[] = await newPage.evaluate(val => { // Run function inside browser context
+        const matches = val.textContent?.matchAll(/「[^「」]*」/g); // Find instances of substrings that contain a quote (see the webpage for why this is done)
 
         if(!matches) return [];
 
         const flattened = Array.from(matches).flat(1);
-        const rawPhrases = flattened.map(val => val.slice(1, val.length - 1).trim().replace(/\s+/g, ""));
+        const rawPhrases = flattened.map(val => val.slice(1, val.length - 1).trim().replace(/\s+/g, "")); // Remove quotes (kagi-kakko) and whitespace from string
         
         return rawPhrases;
-    }, entries);
+    }, entries); // Run the above function on the list div
 
+    //Combine combining characters
+    //This prevents strings that are seemingly duplicates but differ by whether or not the phrase uses combining characters or precombined characters
     const normalizedPhrases = rawPhrases.map(s => normalizeDakuten(s));
     const noDuplicates = Array.from(new Set(normalizedPhrases));
 
